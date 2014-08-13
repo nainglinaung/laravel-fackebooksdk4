fb1b3d229b82ce89aa68bd9729a124ef49976f03
Kyaw Min Lwin <kyaw.min.lwin.squall@gmail.comgit>
2014-08-13 17:45:34 +0630

    updated sdk

--- src/Squall/Facebook/Facebook.php
+++ src/Squall/Facebook/Facebook.php
@@ -2,17 +2,52 @@
 
 namespace Squall\Facebook;
 
+session_start();
+
 use Config;
 
 class Facebook {
 
+    protected $redirectLogin;
+
     public function __construct() {
-        Facebook\FacebookSession::setDefaultApplication(Config::get('facebook::appId'), Config::get('facebook::appSecret'));
+        FacebookSession::setDefaultApplication(Config::get('facebook::appId'), Config::get('facebook::appSecret'));
+        $this->redirectLogin = new FacebookRedirectLoginHelper(Config::get('facebook::redirectLogin'));
+    }
+
+    public function get_accessToken() {
+        return $session = new FacebookSession('access-token');
     }
 
     public function get_tabId() {
-        $helper = new Facebook\FacebookPageTabHelper();
+        $helper = new FacebookPageTabHelper();
         return $helper->getPageId();
+    }
+
+    public function isLiked() {
+        $pageHelper = new FacebookPageTabHelper();
+        return $pageHelper->isLiked();
+    }
+
+    public function get_pageSession() {
+        $pageHelper = new FacebookPageTabHelper();
+        return $pageHelper->getSession();
+    }
+
+    public function get_canvasSession() {
+        $pageHelper = new FacebookCanvasLoginHelper();
+        return $pageHelper->getSession();
+    }
+
+    public function get_grapData($session) {
+
+        $request = new FacebookRequest($this->get_canvasSession(), 'GET', '/me');
+        $response = $request->execute();
+        return $response->getGraphObject()->asArray();
+    }
+
+    public function get_loginUrl() {
+        return $this->redirectLogin->getLoginUrl(Config::get('facebook::scope'));
     }
 
 }
--- src/config/config.php
+++ src/config/config.php
@@ -3,6 +3,7 @@
 return array(
     'appId' => '',
     'appSecret' => '',
+    'redirectLogin' => '',
     'scope' => 'user_birthday,email,user_hometown,user_location,user_status,user_photos,user_likes,user_education_history',
     'locale' => 'en_US'
 );
